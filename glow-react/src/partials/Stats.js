73b3408c704f14345eb9e4cecff3494de7c4baeb
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-07 23:53:19 -0600

    updated statspage with FC information

--- glow-react/src/partials/Stats.js
+++ glow-react/src/partials/Stats.js
@@ -11,23 +11,23 @@ function Stats() {
           <div className="grid grid-cols-2 gap-4 lg:gap-6 md:grid-cols-4 text-center" data-aos-id-stats>
             {/* 1st item */}
             <div className="bg-white dark:bg-gray-800 py-8 px-1 shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-stats]">
-              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">2.4M</div>
-              <div className="text-gray-600 dark:text-gray-400">Days turn around</div>
+              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">Aether</div>
+              <div className="text-gray-600 dark:text-gray-400">Data Center</div>
             </div>
             {/* 2nd item */}
             <div className="bg-white dark:bg-gray-800 py-8 px-1 shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-stats]" data-aos-delay="100">
-              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">7M</div>
-              <div className="text-gray-600 dark:text-gray-400">Days turn around</div>
+              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">Sargatanas</div>
+              <div className="text-gray-600 dark:text-gray-400">World</div>
             </div>
             {/* 3rd item */}
             <div className="bg-white dark:bg-gray-800 py-8 px-1 shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-stats]" data-aos-delay="200">
-              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">7.4%</div>
-              <div className="text-gray-600 dark:text-gray-400">Days turn around</div>
+              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">9/3/2020 </div>
+              <div className="text-gray-600 dark:text-gray-400">Est.</div>
             </div>
             {/* 4th item */}
             <div className="bg-white dark:bg-gray-800 py-8 px-1 shadow-2xl" data-aos="fade-down" data-aos-anchor="[data-aos-id-stats]" data-aos-delay="300">
-              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">49K</div>
-              <div className="text-gray-600 dark:text-gray-400">Days turn around</div>
+              <div className="font-red-hat-display text-3xl font-extrabold tracking-tighter mb-1">53</div>
+              <div className="text-gray-600 dark:text-gray-400">Members</div>
             </div>
           </div>
         </div>
